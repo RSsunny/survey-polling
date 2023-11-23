9e9def35fa521b202aeccc9be106025054b83a97
RSsunny <sunnysharif154@gmail.com>
2023-11-24 03:02:43 +0600

    footer section added also add project name footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,101 @@
+import Container from "../../Shared/Container";
+import Icons from "../Icons/Icons";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer section</h1>
+    <div className="bg-base-200">
+      <Container>
+        <footer className="footer p-10  text-base-content">
+          <aside>
+            <h1 className="text-4xl font-cinzel font-bold  ">
+              Sur<span className="text-primary_Colors text-5xl">v</span>ey
+            </h1>
+            <p>
+              ACME Industries Ltd.
+              <br />
+              Providing reliable tech since 1992
+            </p>
+          </aside>
+          <nav>
+            <header className="footer-title text-primary_Colors ">
+              Services
+            </header>
+            <a className="link hover:text-primary_Colors link-hover   ">
+              Branding
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Design
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Marketing
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Advertisement
+            </a>
+          </nav>
+          <nav>
+            <header className="footer-title text-primary_Colors ">
+              Company
+            </header>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              About us
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Contact
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">Jobs</a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Press kit
+            </a>
+          </nav>
+          <nav>
+            <header className="footer-title text-primary_Colors ">Legal</header>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Terms of use
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Privacy policy
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Cookie policy
+            </a>
+          </nav>
+          <nav>
+            <header className="footer-title text-primary_Colors ">Legal</header>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Terms of use
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Privacy policy
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Cookie policy
+            </a>
+          </nav>
+          <nav>
+            <header className="footer-title text-primary_Colors ">Legal</header>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Terms of use
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Privacy policy
+            </a>
+            <a className="link hover:text-primary_Colors link-hover  ">
+              Cookie policy
+            </a>
+          </nav>
+        </footer>
+        <div className="text-center text-3xl  mx-auto w-[400px] text-primary_Colors my-10">
+          <Icons></Icons>
+        </div>
+        <div className="footer footer-center p-4  text-base-content border-t">
+          <aside>
+            <p className="">
+              Copyright © 2023 - All right reserved by ACME Industries Ltd
+            </p>
+          </aside>
+        </div>
+      </Container>
     </div>
   );
 };
---
+++ src/Shared/Container.jsx
@@ -0,0 +1,7 @@
+const Container = ({ children }) => {
+  return (
+    <div className="max-w-7xl mx-auto px-2 md:px-5 lg:px-0">{children}</div>
+  );
+};
+
+export default Container;
